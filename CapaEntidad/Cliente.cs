c77579fb36f8e00468a88b5bb29f191d8d4272c7
Neil <excellenceneilt@gmail.com>
2023-03-28 16:09:38 -0500

    Se implementaron cambios en el CD_Cliente, frm_Servicio, actulizacion entidad cliente, proceimientos cliente y frm cliente

--- CapaEntidad/Cliente.cs
+++ CapaEntidad/Cliente.cs
@@ -9,31 +9,40 @@ namespace CapaEntidad
     public class Cliente
     {
 
-        //public string  {get;set;}
+
         public int IdCliente { get; set; }
-        public string CodigoCliente {get;set;}
+        public string CodigoCliente { get; set; }
         public string Documento { get; set; } //DNI
+        public string DocumentoContacto { get; set; } //DNI
         public string NombreCompleto { get; set; }
         public string NombreComercial { get; set; }
-        public string Direccion {get;set;}
-        public string DireccionComercial {get;set;}
-        public Tipo_Cliente oTipo_Cliente { get; set;}
+        public string NombreContacto { get; set; }
+        public string Direccion { get; set; }
+        public string DireccionComercial { get; set; }
+        public string DireccionContacto { get; set; }
+        public Tipo_Cliente oTipo_Cliente { get; set; }
         public Tipo_Documento oTipo_Documento { get; set; }
-        public string Departamento {get;set;}
+        public string Departamento { get; set; }
         public string Provincia { get; set; }
         public string Distrito { get; set; }
         public string DepartamentoComercial { get; set; }
         public string ProvinciaComercial { get; set; }
         public string DistritoComercial { get; set; }
-
+        public string DepartamentoContacto { get; set; }
+        public string ProvinciaContacto { get; set; }
+        public string DistritoContacto { get; set; }
         public string Correo1 { get; set; }
-        public string Correo2  { get; set; }
+        public string Correo2 { get; set; }
+        public string CorreoContacto { get; set; }
         public string Telefono1 { get; set; }
         public string Telefono2 { get; set; }
-        public string CMP  { get; set; }
+        public string TelefonofijoContacto { get; set; }
+        public string CelularContacto { get; set; }
+        public string CMP { get; set; }
         public string RazonSocial { get; set; }
         public string RUC { get; set; }
-        public Especialidad oEspecialidad { get; set; }
+        public string RUCContacto { get; set; }
+        // public Especialidad oEspecialidad { get; set; }
         public bool Estado { get; set; }
         public string FechaRegistro { get; set; }
     }
