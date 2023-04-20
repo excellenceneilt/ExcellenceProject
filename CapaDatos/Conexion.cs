3483e534ebea2c9beab4ca9fa766ddc1918563b9
Angel Antonio <excellenceangel77@gmail.com>
2023-04-20 17:55:25 -0500

    Comentando las primeras clases de la capa de datos

--- CapaDatos/Conexion.cs
+++ CapaDatos/Conexion.cs
@@ -9,6 +9,7 @@ namespace CapaDatos
 {
     public class Conexion
     {
+        //
         public static string cadena = ConfigurationManager.ConnectionStrings["cadena_conexion"].ToString();
     }
 }
